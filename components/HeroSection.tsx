745c79204ce5fe69aa58e28e4f9270dbda589f03
JesseOlisa <jessesamuel84@gmail.com>
2023-04-21 11:28:15 -0700

    animation

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { motion, LazyMotion, domAnimation } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const HeroSection = () => {
 	return (
@@ -12,18 +12,16 @@ const HeroSection = () => {
 					<span className='text-[#BF40BF] font-semibold relative drop-shadow-[0_0_0.3rem_#BF40BF60]'>
 						Frontend Developer.
 					</span>
-					<LazyMotion features={domAnimation}>
-						<motion.span
-							initial={{
-								width: '10%',
-							}}
-							animate={{
-								width: '90%',
-							}}
-							// transition={{ ease: 'easeIn', duration: 0.9 }}
-							className='block h-1 mt-1 bg-profile-bg max-w-sm'
-						></motion.span>
-					</LazyMotion>
+					<motion.span
+						initial={{
+							width: '10%',
+						}}
+						animate={{
+							width: '90%',
+						}}
+						// transition={{ ease: 'easeIn', duration: 0.9 }}
+						className='block h-1 mt-1 bg-profile-bg max-w-sm'
+					></motion.span>
 				</h1>
 				<p className='max-w-lg text-[0.9rem] md:text-[1.1rem] opacity-90 mb-3'>
 					I enjoy building various frontend products with great user experience.
@@ -33,21 +31,23 @@ const HeroSection = () => {
 					into real life products.
 				</p>
 			</div>
-			<div className='bg-profile-bg rounded-full p-2'>
-				<motion.div
-					initial={{ scale: 0.95 }}
-					animate={{ scale: 1 }}
-					transition={{ duration: 0.8, ease: 'easeOut' }}
-				>
-					<Image
-						src='/hero_image.png'
-						alt="Jesse's memoji"
-						width={350}
-						height={100}
-						className='bg-black/95 rounded-full'
-					/>
-				</motion.div>
-			</div>
+			{/* <div className='bg-profile-bg rounded-full p-2'> */}
+			<motion.div
+				initial={{ opacity: 0, scale: 0.95 }}
+				animate={{ opacity: 1, scale: 1 }}
+				transition={{ duration: 0.8, ease: 'easeOut' }}
+				className='bg-profile-bg rounded-full p-2'
+			>
+				<Image
+					src='/hero_image.png'
+					alt="Jesse's memoji"
+					width={350}
+					height={100}
+					priority
+					className='bg-black/95 rounded-full'
+				/>
+			</motion.div>
+			{/* </div> */}
 		</section>
 	);
 };
