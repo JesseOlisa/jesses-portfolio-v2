521216501f89e011c4096c248c0bac1dead92229
JesseOlisa <jessesamuel84@gmail.com>
2023-05-05 03:29:27 -0700

    feat: google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import './globals.css';
-import Head from 'next/head';
 import Header from '@/app/components/Header';
 
 export const metadata = {
 	title: 'Jesse Olisa',
 	description:
 		"Welcome to Jesse Olisa's portfolio. Web Developer and Frontend Engineer from West Africa.",
+	verification: {
+		google:
+			'google-site-verification=zSQw_SrYKkUNHr6G84F4JoqUa81ZtmyaFy_44uBOJG8',
+	},
 };
 
 export default function RootLayout({
@@ -15,12 +18,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<Head>
-				<meta
-					name='google-site-verification'
-					content='zSQw_SrYKkUNHr6G84F4JoqUa81ZtmyaFy_44uBOJG8'
-				/>
-			</Head>
 			<body>
 				<Header />
 				{children}
